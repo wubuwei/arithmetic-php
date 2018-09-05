42ecb7a218e5de3c7f8d3b1ba5f108f0641f0984
wubuwei <wubuwei.dev@gmail.con>
2018-09-05 23:51:09 +0800

    add BinaryQuery comment

--- package/Query/BinaryQuery2.php
+++ package/Query/BinaryQuery2.php
@@ -6,7 +6,7 @@ function BinaryQuery(array $container, $search)
     $top = count($container);
     $low = 0;
     while ($low <= $top) {
-        //floor 返回不大于 变量值 的最接近的整数
+        //floor 返回不大于 变量值 的最接近的整数。floor() 返回的类型仍然是 float，因为 float 值的范围通常比 integer 要大。
         //intval 获取变量的 整数值
         $mid = intval(floor(($low + $top) / 2));
         if (!isset($container[$mid])) {
@@ -20,5 +20,10 @@ function BinaryQuery(array $container, $search)
     }
 }
 
+//测试floor
+$flr = floor((3+4)/2);
+$int = intval($flr);
+echo gettype($flr) . "--" . gettype($int);
+echo "</br>";
 
-//递归版 二分查找
+var_dump(BinaryQuery([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 9));
